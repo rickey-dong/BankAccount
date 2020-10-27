4312f4939d43283372b1f8a2dbf956d53d1d327a
Rickey Dong <rickeyd2004@gmail.com>
2020-10-27 18:12:51 -0400

    implemented deposit method

--- BankAccount.java
+++ BankAccount.java
@@ -21,4 +21,16 @@ public class BankAccount
   {
     password = newPass;
   }
+  public boolean deposit(double amount)
+  {
+    if (amount >= 0)
+    {
+      balance = balance + amount;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
 }
